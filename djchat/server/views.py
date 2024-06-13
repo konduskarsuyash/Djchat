bb8bf1b16814486a79827cd21a68fc8db896ecda
konduskarsuyash <suyashkonduskar27@gmail.com>
2024-06-13 07:43:03 +0530

    added a qty parameter so that based upon the qty of server you can filter out categories

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -9,9 +9,13 @@ class ServerListViewSet(viewsets.ViewSet):
     
     def list(self,request):
         category = request.query_params.get('category')
+        qty = request.query_params.get('qty')
         
         if category:
            self.queryset  = self.queryset.filter(category__name=category) #category__name is we are filtering the category's with the help of there name in the server model as it is the Foreign key 
+           
+        if qty :
+            self.queryset = self.queryset[:int(qty)]
             
         serializer = ServerSerializer(self.queryset,many=True) 
     
