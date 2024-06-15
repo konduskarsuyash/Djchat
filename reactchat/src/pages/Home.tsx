7dc5bc9ddbaf47e9350a127c78bc91e2fe31cc75
konduskarsuyash <suyashkonduskar27@gmail.com>
2024-06-16 01:10:36 +0530

    starting of material ui 30th

--- reactchat/src/pages/Home.tsx
+++ reactchat/src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import { Box,CssBaseline } from '@mui/material'
 const Home = () => {
   return (
-    <>
-    <p>Home</p>
-    </>
+  <Box sx={{display:"flex"}}>
+    <CssBaseline/>
+    Home
+  </Box>
   )
 }
 
