083adfa96ec99ebeabd6b6a02aca71ea4fd89aba
konduskarsuyash <suyashkonduskar27@gmail.com>
2024-06-15 19:03:17 +0530

    In the last part we handle the images for categories but this time this is for Server

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -3,6 +3,14 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
 
+
+def server_icon_upload_path(instance,filename):
+    return f"server{instance.id}/server_icon/{filename}"
+
+
+def server_banner_upload_path(instance,filename):
+    return f"server{instance.id}/server_banner/{filename}"
+
 def category_icon_upload_path(instance,filename):
     return f"category{instance.id}/category_icon/{filename}"
 
@@ -52,6 +60,25 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='channel_owner' )
     topic = models.CharField(max_length=100,blank=True)
     server = models.ForeignKey(Server,on_delete=models.CASCADE,related_name="channel_server")
+    banner = models.ImageField(upload_to=server_banner_upload_path,banner=True,blank=True)
+    icon = models.ImageField(upload_to=server_icon_upload_path,banner=True,blank=True) 
+    
+    def save(self,*args,**kwargs):
+        if self.id:
+            existing = get_object_or_404(Category,id=self.id)
+            if existing.icon!=self.icon:
+                existing.icon.delete(save=False)
+            if existing.banner!=self.banner:
+                existing.banner.delete(save=False)
+        super(Category,self).save(*args,**kwargs)
+        
+    @receiver(models.signals.pre_delete,sender="server.Server")
+    def category_delete_files(sender,instance,**kwargs):
+        for field in instance._meta.fields:
+            if field.name == 'icon' or field.name == 'banner':
+                file = getattr(instance,field.name)
+                if file:
+                    file.delete(save=False)
     
     def save(self,*args,**kwargs):
         self.name = self.name.lower()
