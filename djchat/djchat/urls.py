5377070bd4c423ec5ce5c7504980d463b4633b00
konduskarsuyash <suyashkonduskar27@gmail.com>
2024-06-15 15:37:19 +0530

    the issue solved of the api endpoint which was not appearing in th ui it was because of indentation

--- djchat/djchat/urls.py
+++ djchat/djchat/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from django.urls import path
-from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 from server.views import ServerListViewSet
+from rest_framework import routers
 
-router = DefaultRouter()
-router.register('api/server/select',ServerListViewSet )
-
+router = routers.DefaultRouter()
+router.register('api/server/select', ServerListViewSet, basename='server')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/docs/schema',SpectacularAPIView.as_view(),name='schema'),
-    path('api/docs/schema/ui/',SpectacularSwaggerView.as_view()),
+    path('api/docs/schema', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/schema/ui/', SpectacularSwaggerView.as_view()),
 ]+router.urls
