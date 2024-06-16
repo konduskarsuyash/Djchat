323cfed2bc54db40de7e5a4263c0422eedc78bc2
konduskarsuyash <suyashkonduskar27@gmail.com>
2024-06-16 23:06:22 +0530

    responsive fontsize with the help of material ui

--- reactchat/src/theme/theme.tsx
+++ reactchat/src/theme/theme.tsx
@@ -1,4 +1,4 @@
-import { createTheme } from "@mui/material";
+import { createTheme,responsiveFontSizes } from "@mui/material";
 
 declare module "@mui/material/styles"{
     interface Theme{
@@ -34,6 +34,7 @@ export const createMuiTheme = () =>{
             }
         }
     });
+    theme = responsiveFontSizes(theme);
     return theme;
 }
 
