e58510d2068deac2837cd52c99b76128c8e5f84d
konduskarsuyash <suyashkonduskar27@gmail.com>
2024-06-17 00:39:43 +0530

    Controlling the menubar below 600 it should appear and other stuff

--- reactchat/src/pages/template/PrimaryAppBar.tsx
+++ reactchat/src/pages/template/PrimaryAppBar.tsx
@@ -1,32 +1,77 @@
-import { AppBar, Toolbar, Typography, Link } from '@mui/material'
-import React from 'react'
-import {useTheme} from '@mui/material/styles'
+import { AppBar, Box, Toolbar, Typography, Link, IconButton, Drawer, useMediaQuery } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import MenuIcon from "@mui/icons-material/Menu";
 
 const PrimaryAppBar = () => {
+  const [sideMenu, setSideMenu] = useState(false);
   const theme = useTheme();
+
+  const isSmallScreen = useMediaQuery(theme.breakpoints.up("sm")); 
+  // whenever the scrren siz gies above 600px then it gives true 
+
+  useEffect(() =>{
+    if (isSmallScreen && sideMenu){
+      setSideMenu(false);
+    }
+  },[isSmallScreen])
+
+  const handleDrawerToggle = () => {
+    setSideMenu((prevState) => !prevState);
+  };
+
   return (
-    <AppBar sx={{
-      backgroundColor:theme.palette.background.default,
-      borderBottom:`1px solid ${theme.palette.divider}`,
-      }}>
-        <Toolbar variant='dense' 
+    <AppBar
+      sx={{
+        zIndex: (theme) => theme.zIndex.drawer + 2,
+        backgroundColor: theme.palette.background.default,
+        borderBottom: `1px solid ${theme.palette.divider}`,
+      }}
+    >
+      <Toolbar
+        variant="dense"
         sx={{
-          height:theme.primaryAppBar.height,
-          minHeight:theme.primaryAppBar.height,
-        
+          height: theme.primaryAppBar.height,
+          minHeight: theme.primaryAppBar.height,
         }}
+      >
+        <Box sx={{ display: { xs: 'block', sm: 'none' } }}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Box>
+
+        <Drawer
+          anchor="left"
+          open={sideMenu}
+          onClose={handleDrawerToggle}
         >
-          
-        <Link href ='/' underline='none' color='inherit'>
-          <Typography variant='h6' noWrap component="div"
-          sx={{display:{fontWeight:700,letterSpacing:'-0.5px'}}}
+          {[...Array(100)].map((_, i) => (
+            <Typography key={i} paragraph>
+              {i + 1}
+            </Typography>
+          ))}
+        </Drawer>
+
+        <Link href="/" underline="none" color="inherit">
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ fontWeight: 700, letterSpacing: '-0.5px' }}
           >
             DJCHAT
-          </Typography>      
+          </Typography>
         </Link>
-        </Toolbar>
+      </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default PrimaryAppBar 
+export default PrimaryAppBar;
