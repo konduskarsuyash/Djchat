9b6c6688a71df9cb26b5ea4159303ba76bdee524
konduskarsuyash <suyashkonduskar27@gmail.com>
2024-06-16 13:57:43 +0530

    applying custom css in material ui created 1st component

--- reactchat/src/pages/Home.tsx
+++ reactchat/src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Box,CssBaseline } from '@mui/material'
+import PrimaryAppBar from './template/PrimaryAppBar'
+
+
 const Home = () => {
+
   return (
   <Box sx={{display:"flex"}}>
     <CssBaseline/>
+    <PrimaryAppBar/>
     Home
   </Box>
   )
