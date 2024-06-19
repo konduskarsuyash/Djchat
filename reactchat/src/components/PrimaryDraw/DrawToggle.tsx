c255bef3615e97f216fa43926e93979ffbcb39f1
konduskarsuyash <suyashkonduskar27@gmail.com>
2024-06-19 17:37:16 +0530

    customizing the components of material ui as well as the toggle components

--- reactchat/src/components/PrimaryDraw/DrawToggle.tsx
+++ reactchat/src/components/PrimaryDraw/DrawToggle.tsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import { Box, IconButton } from '@mui/material'
-import { ChevronLeft } from '@mui/icons-material'
+import React from 'react';
+import { Box, IconButton } from '@mui/material';
+import { ChevronLeft, ChevronRight } from '@mui/icons-material';
 
+type Props = {
+  open: boolean;
+  handleDrawerOpen: () => void;
+  handleDrawerClose: () => void;
+};
 
-const DrawToggle = () => {
+const DrawToggle: React.FC<Props> = ({
+  open,
+  handleDrawerClose,
+  handleDrawerOpen,
+}) => {
   return (
     <Box sx={{
-        height:'50px',
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-        }}>
-        <IconButton>
-            <ChevronLeft/>
-        </IconButton>
+      height: '50px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <IconButton onClick={open ? handleDrawerClose : handleDrawerOpen}>
+        {open ? <ChevronLeft /> : <ChevronRight />}
+      </IconButton>
     </Box>
-  )
-}
+  );
+};
 
-export default DrawToggle
+export default DrawToggle;
--- reactchat/src/pages/template/PrimaryDraw.tsx
+++ reactchat/src/pages/template/PrimaryDraw.tsx
@@ -1,52 +1,97 @@
-import { Drawer,Box, useMediaQuery,Typography } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import { useTheme } from '@mui/material/styles'
-import DrawToggle from '../../components/PrimaryDraw/DrawToggle'
+import { Box, useMediaQuery, Typography,styled } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import DrawToggle from '../../components/PrimaryDraw/DrawToggle';
+import MuiDrawer from "@mui/material/Drawer"
 
 
 const PrimaryDraw = () => {
-    const theme  = useTheme()
-    const below600 = useMediaQuery("(max-width:599px)")  
-    //below 600 means equal to true
-    const [open,setOpen] = useState(!below600)
+  const theme = useTheme();
+  const below600 = useMediaQuery('(max-width:599px)');
+  const [open, setOpen] = useState(!below600);
 
-    useEffect(()=>{
-        setOpen(!below600)
-    },[below600])
+  const openedMixin = () =>({
+    transition: theme.transitions.create("width",{
+        easing:theme.transitions.easing.sharp,
+        duration:theme.transitions.duration.enteringScreen,
+    }),
+    overflowX:"hidden",
+  })
 
-    const handleDrawerOpen = () =>{
-        setOpen(true);
-    };
 
-    const handleDrawerClose = () =>{
-        setOpen(false);
-    };
+  const closedMixin = () =>({
+    transition: theme.transitions.create("width",{
+        easing:theme.transitions.easing.sharp,
+        duration:theme.transitions.duration.enteringScreen,
+    }),
+    overflowX:"hidden",
+    width:theme.primaryDraw.closed,
+  })
 
+
+  const Drawer = styled(
+    MuiDrawer,
+    {}
+    )(({theme,open}) =>({
+    width:theme.primaryDraw.width,
+    whiteSpace:"nowrap",
+    boxSizing:"border-box",
+    ...(open && {
+        ...openedMixin(),
+        "& .MuiDrawer-paper":openedMixin(),
+    }),
+
+    ...(!open && {
+        ...openedMixin(),
+        "& .MuiDrawer-paper":closedMixin(),
+    }),
+  }));
+
+  useEffect(() => {
+    setOpen(!below600);
+  }, [below600]);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <Drawer open={open} variant={below600?"temporary":"permanent"}
-    PaperProps={{
-        sx:{mt:`${theme.primaryAppBar.height}px`, 
-            height:`calc(100vh - ${theme.primaryAppBar.height}px)`,
-            width:theme.primaryDraw.width,
+    <Drawer
+      open={open}
+      variant={below600 ? 'temporary' : 'permanent'}
+      PaperProps={{
+        sx: {
+          mt: `${theme.primaryAppBar.height}px`,
+          height: `calc(100vh - ${theme.primaryAppBar.height}px)`,
+          width: theme.primaryDraw.width,
         },
-    }}
+      }}
     >
-        <Box>
-            <Box sx={{postion:'absolute',
-                top:0,
-                right:0,
-                width:open?"auto":"100%"}}>
-                <DrawToggle/>
-                {[...Array(50)].map((_, i) => (
-                <Typography key={i} paragraph>
-                {i + 1}
-                </Typography>
-                ))}
-            </Box>
+      <Box>
+        <Box sx={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          width: open ? 'auto' : '100%',
+        }}>
+          <DrawToggle
+            open={open}
+            handleDrawerClose={handleDrawerClose}
+            handleDrawerOpen={handleDrawerOpen}
+          />
+          {[...Array(50)].map((_, i) => (
+            <Typography key={i} paragraph>
+              {i + 1}
+            </Typography>
+          ))}
         </Box>
+      </Box>
     </Drawer>
-  )
-}
+  );
+};
 
-export default PrimaryDraw
+export default PrimaryDraw;
