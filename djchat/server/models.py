ade57a87ea93f91529934593b4d52d0459c52471
konduskarsuyash <suyashkonduskar27@gmail.com>
2024-06-19 19:35:21 +0530

    some error occured in models.py but solved

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -2,96 +2,87 @@ from django.db import models
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
-from .validators import validate_icon_image_size,validate_image_file_extension
+from .validators import validate_icon_image_size, validate_image_file_extension
 
-def server_icon_upload_path(instance,filename):
-    return f"server{instance.id}/server_icon/{filename}"
+def category_icon_upload_path(instance, filename):
+    return f"category/{instance.id}/category_icon/{filename}"
 
-
-def server_banner_upload_path(instance,filename):
+def server_banner_upload_path(instance, filename):
     return f"server{instance.id}/server_banner/{filename}"
 
-def category_icon_upload_path(instance,filename):
-    return f"category{instance.id}/category_icon/{filename}"
-
+def server_icon_upload_path(instance, filename):
+    return f"server/{instance.id}/server_icon/{filename}"
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True,null=True)
+    description = models.TextField(blank=True, null=True)
     icon = models.FileField(
         upload_to=category_icon_upload_path,
         null=True,
         blank=True
-        )
-    
-    def save(self,*args,**kwargs):
+    )
+
+    def save(self, *args, **kwargs):
         if self.id:
-            existing = get_object_or_404(Category,id=self.id)
-            if existing.icon!=self.icon:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
                 existing.icon.delete(save=False)
-        super(Category,self).save(*args,**kwargs)
-        
-    @receiver(models.signals.pre_delete,sender="server.Category")
-    def category_delete_files(sender,instance,**kwargs):
+        super(Category, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender='server.Category')
+    def category_delete_files(sender, instance, **kwargs):
         for field in instance._meta.fields:
             if field.name == 'icon':
-                file = getattr(instance,field.name)
+                file = getattr(instance, field.name)
                 if file:
                     file.delete(save=False)
-        
+
     def __str__(self):
         return self.name
-        
-        
+
 class Server(models.Model):
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='server_owner' )
-    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='server_category')
-    description = models.CharField(max_length=250,null=True,blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='server_owner')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='server_category')
+    description = models.CharField(max_length=250, null=True, blank=True)
     member = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="server_members")
-    
+
     def __str__(self):
         return f"{self.name}/{self.id}"
-        
-    
-    
+
 class Channel(models.Model):
     name = models.CharField(max_length=100)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='channel_owner' )
-    topic = models.CharField(max_length=100,blank=True)
-    server = models.ForeignKey(Server,on_delete=models.CASCADE,related_name="channel_server")
-    banner = models.ImageField(upload_to=server_banner_upload_path,
-     banner=True,
-     blank=True,
-     validators=[validate_image_file_extension]
-     )
-    icon = models.ImageField(upload_to=server_icon_upload_path,
-     banner=True,
-     blank=True,
-     validators=[validate_icon_image_size,validate_image_file_extension]
-     ) 
-    
-    def save(self,*args,**kwargs):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='channel_owner')
+    topic = models.CharField(max_length=100, blank=True)
+    server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="channel_server")
+    banner = models.ImageField(
+        upload_to=server_banner_upload_path,
+        blank=True,
+        validators=[validate_image_file_extension]
+    )
+    icon = models.ImageField(
+        upload_to=server_icon_upload_path,
+        blank=True,
+        validators=[validate_icon_image_size, validate_image_file_extension]
+    )
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
         if self.id:
-            existing = get_object_or_404(Category,id=self.id)
-            if existing.icon!=self.icon:
+            existing = get_object_or_404(Channel, id=self.id)
+            if existing.icon != self.icon:
                 existing.icon.delete(save=False)
-            if existing.banner!=self.banner:
+            if existing.banner != self.banner:
                 existing.banner.delete(save=False)
-        super(Category,self).save(*args,**kwargs)
-        
-    @receiver(models.signals.pre_delete,sender="server.Server")
-    def category_delete_files(sender,instance,**kwargs):
+        super(Channel, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender='server.Channel')
+    def channel_delete_files(sender, instance, **kwargs):
         for field in instance._meta.fields:
             if field.name == 'icon' or field.name == 'banner':
-                file = getattr(instance,field.name)
+                file = getattr(instance, field.name)
                 if file:
                     file.delete(save=False)
-    
-    def save(self,*args,**kwargs):
-        self.name = self.name.lower()
-        super(Channel,self).save(*args,**kwargs)
-    
+
     def __str__(self):
         return self.name
-         
